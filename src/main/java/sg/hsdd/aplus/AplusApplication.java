34fda4db660ef2e75a43fec7183352320d9919a7
hongsc0421 <hongsc0421@gmail.com>
2023-11-04 21:43:43 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/sg/hsdd/aplus/AplusApplication.java
@@ -0,0 +1,13 @@
+package sg.hsdd.aplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplusApplication.class, args);
+	}
+
+}
